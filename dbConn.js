2e233617ccc6adaa18790d1087694bf6fae1ef03
Brian Randle <brianprandle@gmail.com>
2017-05-14 08:41:45 -0500

    Start dbConn class documentation using JSDoc

---
+++ dbConn.js
@@ -0,0 +1,9 @@
+/**
+ * Set connection attributes.  Valid Scope: Before connecting to a database. (or in the callback function of the connection)
+ * The Auto Commit feature is automatically enabled.
+ * @param {number} Attribute is the connection attribute to set. Refer to [this table](http://www-01.ibm.com/support/knowledgecenter/ssw_ibm_i_71/cli/rzadpfnsconx.htm%23rzadpfnsconx__tbcono) for more details. 
+ * @param {(number|string)} Value The value to set the attribute.
+ */
+function setConnAttr(Attribute, Value) {
+
+};
